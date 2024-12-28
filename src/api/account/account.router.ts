f7ac92929c28d1c0638477997d76b225be44fc21
Dokjolly0 <alexviolattolibero.it@gmail.com>
2024-12-28 13:36:30 +0100

    Add user bool

--- src/api/account/account.router.ts
+++ src/api/account/account.router.ts
@@ -4,8 +4,13 @@ import { me, password, username } from "./account.controller";
 import { list } from "./account.controller";
 import { ChangePasswordDTO } from "./account.dto";
 import { validate } from "../../utils/validation-middleware";
+import dotenv from "dotenv";
 
 const router = express.Router();
+dotenv.config();
+
+// Example without users
+// if (process.env.USE_USERS) router.use(isAuthenticated);
 
 router.use(isAuthenticated);
 router.get("/", list);
